22c79216675529c69533349da9162c1b965dea22
xingduo <1451953028@qq.com>
2018-11-26 00:08:49 +0800

    星朵提交：完成product的创建

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from product.models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['productname','productdesc','producter','create_time','id']
+admin.site.register(Product)
