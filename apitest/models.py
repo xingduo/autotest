daa35b2436d3552e29055521d21e4fade291cc04
xingduo <1451953028@qq.com>
2018-11-29 00:04:12 +0800

    星朵提交：完成接口测试model 创建接口测试的前端页面

--- apitest/models.py
+++ apitest/models.py
@@ -6,7 +6,7 @@ class Apitest(models.Model):
 	apitestname = models.CharField('流程接口名称', max_length=64)
 	apitestdesc = models.CharField('描述', max_length=64, null=True)
 	apitester = models.CharField('测试负责人', max_length=16)
-	apitestresult = models.CharField('测试结果')
+	apitestresult = models.BooleanField('测试结果')
 	create_time = models.DateTimeField('创建时间', auto_now=True)
 
 	class Meta:
@@ -16,16 +16,16 @@ class Apitest(models.Model):
 	def __str__(self):
 		return self.apitestname
 
-
 class Apistep(models.Model):
-	Apitest = models.ForeignKey(Apitest)
+	Apitest = models.ForeignKey('Apitest',on_delete=models.CASCADE)
+	apistep = models.CharField('测试步聚', max_length=100, null=True)
 	apiname = models.CharField('接口名称', max_length=100)
 	apiurl = models.CharField('url地址', max_length=200)
 	apiparamvalue = models.CharField('请求参数和值', max_length=800)
 	REQUEST_METHOD = (('get', 'get'), ('post', 'post'), ('put', 'put'), ('delete', 'delete'))
 	apimethod = models.CharField(verbose_name='请求方法', choices=REQUEST_METHOD, default='get', max_length=200, null=True)
 	apiresult = models.CharField('预期结果',max_length=200)
-	apistatus = models.CharField('是否通过')
+	apistatus = models.BooleanField('是否通过')
 	create_time = models.DateTimeField('创建时间',auto_now=True)
 	def __str__(self):
-		return self.name
+		return self.apistep
--- autotest/settings.py
+++ autotest/settings.py
@@ -83,8 +83,9 @@ DATABASES = {
         'PASSWORD':"123456",
         'HOST':"127.0.0.1",
         'PORT':'3306', 
-        'OPTIONS':{'isolation_level':None},
-
+        'OPTIONS':{'isolation_level':None,
+                   'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'",
+                   },
     }
 }
 
